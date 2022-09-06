baf81c47ab66fd18b21c68383702702b3af426aa
saifrahmn <saifrahmn78@gmail.com>
2022-09-06 19:58:52 +0530

    create

---
+++ example.java
@@ -0,0 +1,11 @@
+public class example {
+    public static int solution(int[] arr, int limit, int p){
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        int limit=5;
+        int people = 3; 
+        System.out.println(solution(arr,limit,people));
+    }
+}
---
+++ prob1_3.java
@@ -0,0 +1,5 @@
+public class prob1_3 {
+    public static void main(String args[]){
+
+    }
+}
